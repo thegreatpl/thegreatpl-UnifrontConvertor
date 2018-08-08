7b0c3067ce1e06c91c6b6e5096f34e889e797b20
thegreatpl <imperialjustinian@gmail.com>
2018-08-08 21:35:00 +0100

    commiting working program.

--- UnifontConvertor/Program.cs
+++ UnifontConvertor/Program.cs
@@ -1,15 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace UnifontConvertor
 {
     class Program
     {
+        static Color background = Color.Transparent;
+        static Color foreground = Color.White;
         static void Main(string[] args)
         {
+            Bitmap unifont = new Bitmap("unifont-10.0.07.bmp");
+
+            Dictionary<string, string> hexes = new Dictionary<string, string>(); 
+
+            using (var reader = File.OpenText(@"unifont-10.0.07\font\plane00\unifont-base.hex"))
+            {
+                string str; 
+                while ((str = reader.ReadLine()) != null)
+                {
+                    var split = str.Split(':');
+                    hexes.Add(split[0], split[1]); 
+                }
+            }
+
+            string directory = @"D:\Users\thegreatpl\Desktop\personal projects\Unicode images";// $"{Directory.GetCurrentDirectory()}/output";
+
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory); 
+
+            //31, 63. 
+            int x = 0; 
+            for (int xdx = 32; xdx < unifont.Width; xdx += 16)
+            {
+                int y = 0; 
+                for (int ydx = 64; ydx < unifont.Height; ydx += 16)
+                {
+                    var name = $"{y.ToString("X2")}{x.ToString("X2")}";
+                    Console.WriteLine(name); 
+                    var character = unifont.Clone(new Rectangle(xdx, ydx, 16, 16), unifont.PixelFormat);
+                    var pallete = character.Palette;
+                    pallete.Entries[0] = foreground;
+                    pallete.Entries[1] = background;
+                    character.Palette = pallete;
+
+                    if (hexes.ContainsKey(name) && hexes[name].Length < 33)
+                    {
+                        character = Move16x8ToCenter(character); 
+                    }
+
+                    character.Save($"{directory}/U+{name}.png", System.Drawing.Imaging.ImageFormat.Png);
+                    y++; 
+                }
+                x++; 
+            }
+        }
+
+        public static Bitmap Move16x8ToCenter(Bitmap original)
+        {
+            var newbitmap = new Bitmap(16, 16); 
+            newbitmap.Palette = original.Palette; 
+            for (int xdx = 0; xdx <=8; xdx++)
+            {
+                for (int ydx = 0; ydx < original.Height; ydx++)
+                {
+                    newbitmap.SetPixel(xdx + 4, ydx, original.GetPixel(xdx, ydx)); 
+                }
+            }
+
+            return newbitmap; 
         }
     }
+
+    
 }
